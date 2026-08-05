72999ac9b713d84a26b2e7cd1322e77b0d3db8e1
jy0917 <jiyoonirago0302@gmail.com>
2026-08-05 20:01:17 +0900

    0805_uart_top_tb

--- 0805_uart/sim_1/new/tb_uart_top.v
+++ 0805_uart/sim_1/new/tb_uart_top.v
@@ -2,13 +2,33 @@
 
 module tb_uart_top ();
     
+    parameter BAUD_TICK = (100_000_000 / 9600) * 10;
     reg clk,reset,btn_R;
     wire tx;
-    
+
     uart_controller dut (
         .clk(clk),
         .reset(reset),
         .btn_R(btn_R),
         .tx(tx)
     );
+
+    always #5 clk = ~clk;
+
+    initial begin
+        clk = 0;
+        reset = 1;
+        btn_R = 0;
+        #20;
+        reset = 0;
+
+        #1000;
+        btn_R = 1;
+        #(BAUD_TICK * 10);
+        #10
+        btn_R = 0;
+        #300;
+
+        $stop;
+    end
 endmodule
