462837844818e237e19fbbbfdfecc2576b624337
jy0917 <jiyoonirago0302@gmail.com>
2026-08-12 21:16:20 +0900

    Update sr04.v

--- 0812_hw/sr04.v
+++ 0812_hw/sr04.v
@@ -1,154 +1,146 @@
 `timescale 1ns / 1ps
 
 module sr04_controller (
-    input clk,
-    input reset,
-    input start,
-    input echo,
-    output reg trigger,
-    output done,
-    output [8:0] distance
+    input                            clk,
+    input                            reset,
+    input                            start,
+    input                            echo,
+    output [$clog2(40_000/58) - 1:0] distance,
+    output                           trigger,
+    output                           done
 );
-    wire w_tick_us;
-    //fsm control_unit
-    //5개니까 3비트로
-    localparam [2:0] IDLE = 0, START = 1, WAIT = 2, COUNT = 3, DISTANCE = 4;
-    reg [2:0] c_state, n_state;
-    reg run_stop, clear;
-    reg [$clog2(58*400)-1:0] counter_reg, counter_next;
+
+    parameter IDLE = 0, START = 1, ECHO_WAIT = 2, ECHO_COUNT = 3;
+
+    reg [$clog2(40_000) - 1:0] count_reg, count_next;
+    reg [1:0] state_c, state_n;
     reg done_reg, done_next;
-    reg [8:0] distance_reg, distance_next;
+    reg [$clog2(40_000) - 1:0] echo_pulse_width_reg, echo_pulse_width_next;
+    reg run_stop_reg, run_stop_next;
+    reg clear_reg, clear_next;
+    reg trigger_reg, trigger_next;
+    wire w_tick_us, w_clear, w_run_stop;
 
+    assign trigger = (state_c == START);
     assign done = done_reg;
-    assign distance = distance_reg;
+    assign distance = echo_pulse_width_reg / 58;
+    assign w_run_stop = run_stop_reg;
+    assign w_clear = clear_reg;
 
-    tick_us U_TICK_US (
+    tick_gen_us U_TICK_GEN_US (
         .clk(clk),
         .reset(reset),
-        .run_stop(run_stop),
-        .clear(clear),
-        .o_tick_us(w_tick_us)
+        .run_stop(w_run_stop),
+        .clear(w_clear),
+        .tick_us(w_tick_us)
     );
+
 
     always @(posedge clk, posedge reset) begin
         if (reset) begin
-            c_state <= IDLE;
-            counter_reg <= 0;
-            done_reg <= 1'b0;
-            distance_reg <= 9'd0;
+            state_c <= IDLE;
+            count_reg <= 0;
+            done_reg <= 0;
+            echo_pulse_width_reg <= 0;
+            run_stop_reg <= 0;
+            clear_reg <= 1;
+            trigger_reg <= 0;
         end else begin
-            c_state <= n_state;
-            counter_reg <= counter_next;
+            state_c <= state_n;
+            count_reg <= count_next;
             done_reg <= done_next;
-            distance_reg <= distance_next;
+            echo_pulse_width_reg <= echo_pulse_width_next;
+            run_stop_reg <= run_stop_next;
+            clear_reg <= clear_next;
+            trigger_reg <= trigger_next;
         end
     end
 
-    //next , output
-    always @(*) begin
-        n_state = c_state;
-        counter_next = counter_reg;
-        done_next = done_reg;
-        distance_next = distance_reg;
-        run_stop = 1'b0;
-        clear = 1'b0;
-        trigger = 1'b0;
-        case (c_state)
+    //  일단 runstop, clear는 무시
+    always @* begin
+        state_n = state_c;
+        count_next = count_reg;
+        done_next = 0;
+        echo_pulse_width_next = echo_pulse_width_reg;
+        run_stop_next = run_stop_reg;
+        clear_next = clear_reg;
+        trigger_next = 0;
+        case (state_c)
             IDLE: begin
-                run_stop = 1'b0;
-                clear = 1'b1;
-                if (start) begin
-                    n_state = START;
-                    counter_next = 0;
-                end
+                count_next = 0;
+                clear_next = 1;
+                run_stop_next = 0;
+                if (start) state_n = START;
             end
-            //start에선 trigger생성. tick_cnt동작시키게 
-            //tick_us : run_stop=1 , clear = 0, trigger = 1
             START: begin
-                run_stop = 1'b1;
-                clear = 1'b0;
-                trigger = 1'b1;
+                clear_next = 0;
+                run_stop_next = 1;
+                trigger_next = 1;
                 if (w_tick_us) begin
-                    //start에서 wait로 가려면 tick이 10번 필요 -> tick_cnt 필요
-                    counter_next = counter_reg + 1;
-                end
-                //시작하자마자 틱이 생성되는게 아니기 때문에 10이 아닌 11
-                //다음으로 바뀌려면 tick_cnt = 11
-                if (counter_reg == 11) begin
-                    counter_next = 0;
-                    n_state = WAIT;
-                end
-            end
-            WAIT: begin
-                run_stop = 1'b1;
-                clear = 1'b0;
-                trigger = 1'b0;
-                if (w_tick_us) begin
-                    if (echo) begin
-                        counter_next = counter_reg + 1;
-                        n_state = COUNT;
-                    end
-                end
-                //test
-                // n_state = IDLE;
-                // run_stop = 1'b0;
-                // clear = 1'b0;
-                // trigger = 1'b0;
-            end
-            COUNT: begin
-                run_stop = 1'b1;
-                clear = 1'b0;
-                trigger = 1'b0;
-                if (w_tick_us) begin
-                    if (echo) begin
-                        counter_next = counter_reg + 1; //echo=1인 동안 계속 카운트
+                    if (count_reg == 9) begin
+                        count_next = 0;
+                        state_n = ECHO_WAIT;
                     end else begin
-                        n_state = DISTANCE; /// echo=0으로 떨어지면 종료
+                        count_next = count_reg + 1;
                     end
                 end
             end
-            DISTANCE: begin
-                distance_next = counter_reg / 58;
-                done_next = 1'b1;
-                n_state = IDLE;
+            ECHO_WAIT: begin
+                if (w_tick_us && echo) begin
+                    count_next = count_reg + 1;
+                    state_n = ECHO_COUNT;
+                end
+            end
+            ECHO_COUNT: begin
+                if (w_tick_us) begin
+                    if (echo) begin
+                        if (count_reg == 36_000 - 1) begin // ← echo가 36ms까지도 안 떨어지면 0으로 침
+                            echo_pulse_width_next = 0;
+                            done_next = 1;
+                            state_n = IDLE;
+                        end
+                        count_next = count_reg + 1;
+                    end else begin
+                        echo_pulse_width_next = count_reg; // ← echo가 정상적으로 떨어지면 실제 카운트값 사용
+                        done_next = 1;
+                        state_n = IDLE;
+                    end
+                end
             end
         endcase
     end
 
-
 endmodule
 
-module tick_us (
+
+
+module tick_gen_us (
     input  clk,
     input  reset,
     input  run_stop,
     input  clear,
-    output o_tick_us
+    output tick_us
 );
-    //100mhz
-    parameter F_COUNT = 100;
-
-    reg [$clog2(F_COUNT)-1:0] counter_reg;
     reg tick_us_reg;
+    reg [$clog2(100) - 1:0] count_reg;
 
-    assign o_tick_us = tick_us_reg;
-
+    assign tick_us = tick_us_reg;
 
     always @(posedge clk, posedge reset) begin
         if (reset | clear) begin
-            counter_reg <= 0;
             tick_us_reg <= 0;
+            count_reg   <= 0;
         end else begin
             if (run_stop) begin
-                counter_reg <= counter_reg + 1;
-                if (counter_reg == (F_COUNT - 1)) begin
-                    counter_reg <= 0;
-                    tick_us_reg <= 1'b1;
+                if (count_reg == 99) begin
+                    count_reg   <= 0;
+                    tick_us_reg <= 1;
                 end else begin
-                    tick_us_reg <= 1'b0;
+                    count_reg   <= count_reg + 1;
+                    tick_us_reg <= 0;
                 end
+
             end
         end
     end
-
 endmodule
